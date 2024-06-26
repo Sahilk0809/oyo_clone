e8a932693983fa206b43897dacafc8d30cf94452
niksnimje <niksnimje4@gamil.com>
2024-06-26 15:08:15 +0530

    Prachi

---
+++ lib/components/Splashscreen/Splash_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Splash_screen extends StatefulWidget {
+  const Splash_screen({super.key});
+
+  @override
+  State<Splash_screen> createState() => _Splash_screenState();
+}
+
+class _Splash_screenState extends State<Splash_screen> {
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: height*0.3,
+            width: width*0,
+          ),
+        ],
+      ),
+    );
+  }
+}
