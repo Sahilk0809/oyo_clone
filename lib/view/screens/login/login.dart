84d595a061137d5125912b9922fb78adc87153ad
Vishal Keshri <vishalkeshri858@gmail.com>
2024-06-26 15:45:36 +0530

    vishal

--- lib/view/screens/login/login.dart
+++ lib/view/screens/login/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../../utils/global.dart';
+import 'component/component.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -28,104 +29,7 @@ class _LoginScreenState extends State<LoginScreen> {
                     ),
                   ),
                 ),
-                DropdownButton(
-                  itemHeight: 50,
-                  value: dropDownSelectedIndex,
-                  borderRadius: BorderRadius.circular(20),
-                  focusColor: const Color(0xff2a1710),
-                  icon: const Icon(
-                    Icons.keyboard_arrow_down_outlined,
-                    color: Color(0xff8e8d8d),
-                    size: 25,
-                  ),
-                  items: const [
-                    DropdownMenuItem(
-                      value: 0,
-                      child: Text(
-                        "    India                                   ",
-                        style: TextStyle(
-                            color: Color(0xff8e8d8d),
-                            fontWeight: FontWeight.w400,
-                            fontSize: 17),
-                      ),
-                    ),
-                    DropdownMenuItem(
-                      value: 1,
-                      child: Text(
-                        "hii",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xff2a1710),
-                            fontSize: 17),
-                      ),
-                    ),
-                    DropdownMenuItem(
-                      value: 2,
-                      child: Text(
-                        "Chocolate",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xff2a1710),
-                            fontSize: 17),
-                      ),
-                    ),
-                    DropdownMenuItem(
-                      value: 3,
-                      child: Text(
-                        "Cappuccino",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xff2a1710),
-                            fontSize: 17),
-                      ),
-                    ),
-                    DropdownMenuItem(
-                      value: 4,
-                      child: Text(
-                        "Ice Coffee",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xff2a1710),
-                            fontSize: 17),
-                      ),
-                    ),
-                    DropdownMenuItem(
-                      value: 5,
-                      child: Text(
-                        "Hot Coffee",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xff2a1710),
-                            fontSize: 17),
-                      ),
-                    ),
-                    DropdownMenuItem(
-                      value: 6,
-                      child: Text(
-                        "Cold Cooffee",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xff2a1710),
-                            fontSize: 17),
-                      ),
-                    ),
-                    DropdownMenuItem(
-                      value: 7,
-                      child: Text(
-                        "Chocolate Drinks",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xff2a1710),
-                            fontSize: 17),
-                      ),
-                    ),
-                  ],
-                  onChanged: (value) {
-                    setState(() {
-                      dropDownSelectedIndex = value!;
-                    });
-                  },
-                ),
+                DropdownButtonExample(),
               ],
             )
           ],
