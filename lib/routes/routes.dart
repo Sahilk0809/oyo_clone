905d11c84c021a4e4edf2fbf6c32140b8a2d9011
Sahil <rk4852115@gmail.com>
2024-06-27 15:27:50 +0530

    Sahil

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:oyo_clone/view/screens/homescreen/homescreen.dart';
+import 'package:oyo_clone/view/screens/savescreen/savescreen.dart';
 import '../view/screens/login/login.dart';
 import '../view/screens/servicescreen/servicescreen.dart';
 
 class MyRoutes{
   static Map<String, Widget Function(BuildContext)> routes = {
     //  '/': (context) => const Homescreen(),
-    '/': (context) => const LoginScreen(),
+    // '/': (context) => const LoginScreen(),
     //'/':(context)=>Service_page(),
+    '/': (context) => const SavedScreen(),
   };
 }
---
+++ lib/view/screens/savescreen/savescreen.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+class SavedScreen extends StatefulWidget {
+  const SavedScreen({super.key});
+
+  @override
+  State<SavedScreen> createState() => _SavedScreenState();
+}
+
+class _SavedScreenState extends State<SavedScreen> {
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        leading: const Icon(
+          Icons.arrow_back,
+        ),
+        backgroundColor: Colors.white,
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(left: 15, top: 28),
+                child: const Text(
+                  'Your saved OYOs',
+                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 150,
+          ),
+          Column(
+            children: [
+              Image.asset(
+                'asset/banglore.jpeg',
+                height: 255,
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 85,
+          ),
+          Column(
+            children: [
+              Text(
+                'No saved OYOs, yet!',
+                style: TextStyle(
+                    fontSize: 23,
+                    fontWeight: FontWeight.bold,
+                    height: height * 0.0018),
+              ),
+              Text(
+                'Start searching for OYO rooms now',
+                style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.grey.shade600,
+                    letterSpacing: -0.1,
+                    height: 2),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 10),
+                height: height * 0.059,
+                width: width * 0.390,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Center(
+                  child: Text(
+                    'Explore OYOs',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
